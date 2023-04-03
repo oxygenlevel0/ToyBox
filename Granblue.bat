ee1dd20cd8e2c4440fbb25b4c11d83d0c3281a58
oxygenlevel0 <50875823+oxygenlevel0@users.noreply.github.com>
2023-04-03 12:50:06 +0900

    Update Granblue.bat

--- Granblue.bat
+++ Granblue.bat
@@ -1,5 +1,11 @@
 @echo off
 
+:: コマンドプロンプトを最小化して起動する
+if not "%~0" == "%~dp0.\%~nx0" (
+    start /min cmd /c,"%~dp0.\%~nx0" %*
+    exit
+)
+
 start chrome_proxy.exe^
     --user-data-dir = C:\Users\hoge\hoge^
     --profile-directory = "Profile hoge"^
