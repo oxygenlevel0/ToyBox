635788cafe5d92734f6b2150ef42ce9f10861f6e
oxygenlevel0 <50875823+oxygenlevel0@users.noreply.github.com>
2023-04-03 12:50:32 +0900

    Update Granblue.bat

--- Granblue.bat
+++ Granblue.bat
@@ -13,6 +13,5 @@ start chrome_proxy.exe^
     --window-position = hoge,hoge^
     --app = "https://game.granbluefantasy.jp/#mypage"
 
-:: pause.exe使用
 timeout /t 1 /nobreak >nul
 sendkeys /active [Ctrl]p
