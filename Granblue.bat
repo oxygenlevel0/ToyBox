8db8bbd3f0e8a7ce72d18b64bbc504574f547db9
oxygenlevel0 <50875823+oxygenlevel0@users.noreply.github.com>
2023-04-03 12:05:37 +0900

    Update Granblue.bat

--- Granblue.bat
+++ Granblue.bat
@@ -1,11 +1,11 @@
 @echo off
 
 start chrome_proxy.exe^
-    --user-data-dir=C:\Users\hoge\hoge^
-    --profile-directory="Profile hoge"^
-    --window-size=hoge,hoge^
-    --window-position=hoge,hoge^
-    --app="https://game.granbluefantasy.jp/#mypage"
+    --user-data-dir = C:\Users\hoge\hoge^
+    --profile-directory = "Profile hoge"^
+    --window-size = hoge,hoge^
+    --window-position = hoge,hoge^
+    --app = "https://game.granbluefantasy.jp/#mypage"
 
 :: pause.exe使用
 timeout /t 1 /nobreak >nul
