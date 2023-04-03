d57e62628e23d9c200d32467ae3d4caae59cb8f7
oxygenlevel0 <50875823+oxygenlevel0@users.noreply.github.com>
2023-04-03 12:01:05 +0900

    Update Granblue.bat

--- Granblue.bat
+++ Granblue.bat
@@ -1,13 +1,11 @@
 @echo off
 
-start chrome_proxy.exe ^
+start chrome_proxy.exe^
+    --user-data-dir=C:\Users\ryuji\guraburu^
+    --profile-directory="Profile 1"^
+    --window-size=336,632^
+    --window-position=1580,408^
+    --app="https://game.granbluefantasy.jp/#mypage"
 
---user-data-dir=C:\Users\hoge\hoge ^
---profile-directory = "Profile 1" ^
---window-size = int,int ^
---window-position = int,int ^
---app = "https://game.granbluefantasy.jp/#mypage"
-
-:: sleep.exe,最前面でポーズ使用
-sleep int
+timeout /t 1 /nobreak >nul
 sendkeys /active [Ctrl]p
