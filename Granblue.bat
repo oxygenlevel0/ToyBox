391caf5a4ae82f965d7755af66e6b2e9a247abb6
oxygenlevel0 <50875823+oxygenlevel0@users.noreply.github.com>
2023-04-05 18:51:38 +0900

    Update Granblue.bat

--- Granblue.bat
+++ Granblue.bat
@@ -8,14 +8,14 @@ if not "%Gran%"=="Djeeta" (
 )
 
 :: Chromeを起動(2行目からはオプション)
-start chrome_proxy.exe^
+start chrome.exe^
     --user-data-dir=C:\Users\hoge\hoge^
     --profile-directory="Profile hoge"^
     --window-size=hoge,hoge^
     --window-position=hoge,hoge^
     --app="https://game.granbluefantasy.jp/#mypage"
 
-timeout /t 1 /nobreak >nul
-:: SendKeysでPause.exeのショートカットを送信
-sendkeys /active pause
+    timeout /t 1 /nobreak >nul
+    :: SendKeysでPause.exeのショートカットを送信
+    sendkeys /active pause
 exit
